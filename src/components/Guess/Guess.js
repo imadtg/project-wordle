fa002af6efdc8ddcc5d5cb472a5ecb7e73b95934
imadtg <i.haidi@outlook.com>
2024-06-15 14:59:12 +0100

    ex4

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,13 +1,22 @@
 import { range } from "../../utils";
+import { checkGuess } from "../../game-helpers";
 import React from "react";
 
-function Guess({ value }) {
+function Cell({ letter, status }) {
+  let className = status ? `cell ${status}` : "cell";
+  return <span className={className}>{letter}</span>;
+}
+
+function Guess({ value, answer }) {
+  let feedback = checkGuess(value, answer);
   return (
     <p class="guess">
       {range(5).map((num) => (
-        <span key={num} className="cell">
-          {value ? value[num] : undefined}
-        </span>
+        <Cell
+          key={num}
+          letter={value ? value[num] : undefined}
+          status={feedback ? feedback[num].status : undefined}
+        />
       ))}
     </p>
   );
